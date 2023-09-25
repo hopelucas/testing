6ca1abbe6ac76e633de1be09822793e8806f5e2b
Hope Lucas <animad_hope@hotmail.co.uk>
2023-09-25 23:15:12 +0930

    Create delete.js

---
+++ src/commands/delete.js
@@ -0,0 +1,8 @@
+const { REST, Routes } = require('discord.js');
+const { CLIENT_ID, GUILD_ID, TOKEN } = require('./env');
+
+const rest = new REST().setToken(TOKEN);
+
+rest.delete(Routes.applicationCommand(CLIENT_ID, '1151130693301178389'))
+	.then(() => console.log('Successfully deleted application command'))
+	.catch(console.error);
